acea908926996f22691f3a1886dc0dfbde36180a
Erik Marsh <erik.i.marsh@gmail.com>
2021-04-17 21:47:42 -0700

    webcam integration for front-facing cameras

---
+++ Assets/Webcam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webcam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var devices = WebCamTexture.devices;
+        foreach (WebCamDevice d in devices)
+		{
+            Debug.Log(d.name);
+		}
+
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+
+        WebCamTexture tex = new WebCamTexture("e2eSoft iVCam");
+        rend.material.mainTexture = tex;
+        tex.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# CS484 Semester Project - Study Focuser
+
+Created by Erik Marsh and Sean O'Kelly
+
+## Dependenices
+* Unity 2019.4.22f1
+* SteamVR 2.7.3
+  * Using UnityXR support
