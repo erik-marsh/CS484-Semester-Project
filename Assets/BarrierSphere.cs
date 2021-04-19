068a7ff9bac6ac10129e8f4213dddc1166d06cf9
Erik Marsh <erik.i.marsh@gmail.com>
2021-04-18 20:02:06 -0700

    set up shaders for cutting holes in the barrier sphere

---
+++ Assets/BarrierSphere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierSphere : MonoBehaviour
+{
+	public GameObject barrierSphere;
+
+	private void Awake()
+	{
+		// invert sphere normals so we can texture the inside
+		Vector3[] normals = barrierSphere.GetComponent<MeshFilter>().mesh.normals;
+		for (int i =0; i < normals.Length; i++)
+		{
+			normals[i] = -normals[i];
+		}
+		barrierSphere.GetComponent<MeshFilter>().mesh.normals = normals;
+	}
+}
